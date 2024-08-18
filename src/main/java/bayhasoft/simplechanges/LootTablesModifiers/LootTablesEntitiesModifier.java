2d505ebc4af368dfaa84450b0324b8623bcf2d55
KJ7GDS <gerritbayha@hotmail.com>
2024-08-18 16:19:49 -0500

    port to 1.21

--- src/main/java/bayhasoft/simplechanges/LootTablesModifiers/LootTablesEntitiesModifier.java
+++ src/main/java/bayhasoft/simplechanges/LootTablesModifiers/LootTablesEntitiesModifier.java
@@ -1,7 +1,7 @@
 package bayhasoft.simplechanges.LootTablesModifiers;
 
 import bayhasoft.simplechanges.ModItems;
-import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
+import net.fabricmc.fabric.api.loot.v3.LootTableEvents;
 import net.minecraft.entity.EntityType;
 import net.minecraft.item.Items;
 import net.minecraft.loot.LootPool;
@@ -15,14 +15,14 @@ import net.minecraft.loot.provider.number.UniformLootNumberProvider;
 
 public class LootTablesEntitiesModifier {
     public static void modifyLootTables() {
-        LootTableEvents.MODIFY.register((key, tableBuilder, source) -> {
+        LootTableEvents.MODIFY.register((key, tableBuilder, source, registries) -> {
+            
             if(EntityType.PIG.getLootTableId() == key) {
                 LootPool.Builder poolBuilder = LootPool.builder()
                     .rolls(ConstantLootNumberProvider.create(1))
                     .with(ItemEntry.builder(ModItems.pig_skin))
                     .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(0.0f, 2.0f)).build())
-                    //.apply(EnchantedCountIncreaseLootFunction.builder(null, UniformLootNumberProvider.create(0.0F, 1.0F)))/*TODO: make this line work(looting Enchantment) */
-                    /*.apply(LootingEnchantLootFunction.builder(UniformLootNumberProvider.create(0, 1)))*/;
+                    .apply(EnchantedCountIncreaseLootFunction.builder(registries, UniformLootNumberProvider.create(0, 1)));
                 tableBuilder.pool(poolBuilder.build());
             }
 
@@ -31,26 +31,19 @@ public class LootTablesEntitiesModifier {
                     .rolls(ConstantLootNumberProvider.create(1))
                     .with(ItemEntry.builder(Items.GLOWSTONE_DUST))
                     .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(0.0f, 2.0f)).build())
-                    /* .apply(LootingEnchantLootFunction.builder(UniformLootNumberProvider.create(0, 1)))*/;
+                    .apply(EnchantedCountIncreaseLootFunction.builder(registries, UniformLootNumberProvider.create(0, 1)));
                 tableBuilder.pool(poolBuilder.build());
             }
-            
             
             if(EntityType.HUSK.getLootTableId() == key) {
                 LootPool.Builder poolBuilder = LootPool.builder()
                     .rolls(ConstantLootNumberProvider.create(1))
-                    .conditionally(RandomChanceLootCondition.builder(.20f))
+                    .conditionally(RandomChanceLootCondition.builder(.50f))
                     .with(ItemEntry.builder(Items.SAND))
-                    .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 1.0f)).build())
-                    /*.apply(LootingEnchantLootFunction.builder(UniformLootNumberProvider.create(0, 1)))*/;
+                    .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 2.0f)).build())
+                    .apply(EnchantedCountIncreaseLootFunction.builder(registries, UniformLootNumberProvider.create(0, 1)));
                 tableBuilder.pool(poolBuilder.build());
             }
-            /*---------------------------------------------------------- */
-
-            /*---------------------------------------------------------- */
         });
     }
-    /*---------------------------------------------------------- */
-
-    /*---------------------------------------------------------- */
 }
